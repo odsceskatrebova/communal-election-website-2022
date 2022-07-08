ef262fe7fd889aabf77208343166fda96421dfd8
lubomirfiala <kontakt@lubomirfiala.com>
2022-07-08 09:27:58 +0200

    default page

--- assets/website/js/header.js
+++ assets/website/js/header.js
@@ -2,11 +2,13 @@ const headerWrapper = document.querySelector('.header-wrapper');
 // const headerFixer = document.querySelector('.header-fixer');
 
 document.addEventListener('scroll', (e)=>{
-  let rect = headerWrapper.getBoundingClientRect();
-  console.log(headerWrapper.offsetTop);
-  if(rect.top < -80 ) {
-    headerWrapper.classList.add('fixed');
-  } else {
-    headerWrapper.classList.remove('fixed');
+  if(headerWrapper.classList.contains('homepage')) {
+    let rect = headerWrapper.getBoundingClientRect();
+    console.log(headerWrapper.offsetTop);
+    if(rect.top < -80 ) {
+      headerWrapper.classList.add('fixed');
+    } else {
+      headerWrapper.classList.remove('fixed');
+    }
   }
 });
