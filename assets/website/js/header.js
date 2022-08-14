0769aabae818f6b67f2966ce59dfbcf50ee3a05f
lubomirfiala <kontakt@lubomirfiala.com>
2022-08-14 17:44:04 +0200

    menu button

--- assets/website/js/header.js
+++ assets/website/js/header.js
@@ -1,6 +1,7 @@
 const headerWrapper = document.querySelector('.header-wrapper');
 const menuToggl = document.querySelector('.menu-toggl');
 const nav = document.querySelector('nav');
+const navButtons = nav.querySelectorAll('.nav-btn');
 // const headerFixer = document.querySelector('.header-fixer');
 
 document.addEventListener('scroll', (e)=>{
@@ -18,4 +19,13 @@ document.addEventListener('scroll', (e)=>{
 
 menuToggl.addEventListener('click', () => {
   nav.classList.toggle('show');
+  menuToggl.classList.toggle('show');
 });
+
+navButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    nav.classList.remove('show');
+    menuToggl.classList.remove('show');
+  });
+});
+
